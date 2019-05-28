2b256df19e3a39183a7d4c6ec50e467f2145105f
Mior Muhammad Zaki <crynobone@gmail.com>
2019-05-28 18:27:17 +0800

    Update config docblock.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/remote-control.php
+++ config/remote-control.php
@@ -1,13 +1,30 @@
 <?php
 
 return [
+    /*
+     |--------------------------------------------------------------------------
+     | Hash Key
+     |--------------------------------------------------------------------------
+     |
+     | Define the project hash key for remote control. It will fallback to
+     | the application default key.
+     |
+     */
+
     'key' => env('REMOTE_CONTROL_KEY') ?? env('APP_KEY'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Database configuration
+     |--------------------------------------------------------------------------
+     |
+     | Define the project database configuration to manage remote control.
+     |
+     */
 
     'database' => [
         'table' => 'user_remote_controls',
 
         'connection' => null,
     ],
-
-    'mailable' => RemoteControl\Mail\GrantRemoteAccess::class,
 ];
